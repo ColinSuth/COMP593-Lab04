30d1303f42f48e652ac7b5d3e32d72d53aabc37d
ColinSuth <colintgsutherland@gmail.com>
2023-02-14 16:39:20 -0500

    Lab04
    Lab04 Finished

--- lab4.py
+++ lab4.py
@@ -1,13 +1,15 @@
 from log_analysis import get_log_file_path_from_cmd_line, filter_log_by_regex
 import pandas as pd
+import re
 
 def main():
     log_file = get_log_file_path_from_cmd_line(1)
     port_traffic = tally_port_traffic(log_file)
+    generate_invalid_user_report(log_file)
+    generate_source_ip_log(log_file, ip_address='220.195.35.40')
 
     for port_num, count in port_traffic.items():
         if count >= 100:
-            generate_invalid_user_report(log_file)
             generate_port_traffic_report(log_file, port_num)
 
     pass
@@ -43,7 +45,12 @@ def generate_invalid_user_report(log_file):
 
 # TODO: Step 12
 def generate_source_ip_log(log_file, ip_address):
-
+    regex = r'(.*SRC=' + f'{ip_address}' + r'.*)'
+    data = filter_log_by_regex(log_file, regex)[1]
+    
+    report_df = pd.DataFrame(data)
+    ip_address = re.sub(r'\.', '_', ip_address)
+    report_df.to_csv(f'source_ip_{ip_address}.log', index=False, header=False)
     return
 
 if __name__ == '__main__':
